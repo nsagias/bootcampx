4cbf5243f312c6399c1e7d5c7cd2129fe381b17b
Nick <nsagias@gmail.com>
2021-10-12 18:11:03 -0400

    Add assignments and assignment submissions

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,19 @@
+-- \i migrations/assignments_submissions.sql
+-- \i seeds/assignments_seeds.sql
+-- \i seeds/assignment_submissions_seeds.sql
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
+);
