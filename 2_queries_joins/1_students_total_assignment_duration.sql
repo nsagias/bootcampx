89ffce02f6599c84ed1ab22c3c17c5f5e3eddaf9
Nick <nsagias@gmail.com>
2021-10-12 22:27:40 -0400

    Add students total assignment duration

---
+++ 2_queries_joins/1_students_total_assignment_duration.sql
@@ -0,0 +1,12 @@
+-- \i 2_queries_joins/1_students_total_assignment_duration.sql
+
+
+SELECT 
+  sum(assignment_submissions.duration)
+FROM 
+  students
+JOIN 
+  assignment_submissions ON student_id = students.id
+WHERE 
+  students.name = 'Ibrahim Schimmel';
+ 
