f934026c1c8077e09aec070760edd051d754e574
Nick <nsagias@gmail.com>
2021-10-12 23:42:54 -0400

    get all assignments grouped by day

---
+++ 3_queries_group-by/1_assignments_per_day.sql
@@ -0,0 +1,19 @@
+-- \i 3_queries_group-by/1_assignments_per_day.sql
+
+-- Get the total number of assignments for each day of bootcamp.
+
+-- Select the day and the total assignments.
+-- Order the results by day.
+-- This query only requires the assignments table.
+
+
+-- expected output  day | total_assignments 51 rows
+SELECT 
+  count(name) as total_assignments
+FROM 
+  assignments
+GROUP BY 
+  day;
+
+
+
