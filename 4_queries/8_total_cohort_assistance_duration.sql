b72c6eda112ffd7d1b61524c2c784169a6b940b9
Nick <nsagias@gmail.com>
2021-10-14 16:53:40 -0400

    add total_cohort not complete, update 5_average...

---
+++ 4_queries/8_total_cohort_assistance_duration.sql
@@ -0,0 +1,9 @@
+-- \i 4_queries/8_total_cohort_assistance_duration.sql
+
+-- expected cohort | total_duration 
+SELECT cohorts.name, avg(completed_at - started_at) as total_duration
+FROM assistance_requests 
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_duration;
