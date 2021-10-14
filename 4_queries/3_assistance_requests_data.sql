38b6d70ccbc334d3b55ada5063739c66007a12a8
Nick <nsagias@gmail.com>
2021-10-14 15:27:22 -0400

    Use course provided solution

--- 4_queries/3_assistance_requests_data.sql
+++ 4_queries/3_assistance_requests_data.sql
@@ -19,26 +19,26 @@
 --  Roberto Towne      | Maximillia Willms        | Quibusdam est    | 00:03:00
 -- (26299 rows)
 
-SELECT
-   teachers.name as teacher,
-   students.name as student,
-   assignments.name as assignment,
-   (completed_at - started_at) as duration
-FROM
-   assistance_requests
-JOIN
-  students ON student_id  = students.id 
-JOIN 
-  teachers ON teacher_id =  teachers.id
-JOIN 
-  assignments ON assignments.id = assignment_id
-ORDER BY 
-  duration
-;
+-- SELECT
+--    teachers.name as teacher,
+--    students.name as student,
+--    assignments.name as assignment,
+--    (completed_at - started_at) as duration
+-- FROM
+--    assistance_requests
+-- JOIN
+--   students ON student_id  = students.id 
+-- JOIN 
+--   teachers ON teacher_id =  teachers.id
+-- JOIN 
+--   assignments ON assignments.id = assignment_id
+-- ORDER BY 
+--   duration
+-- ;
 
--- SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (completed_at-started_at) as duration
--- FROM assistance_requests
--- JOIN teachers ON teachers.id = teacher_id
--- JOIN students ON students.id = student_id
--- JOIN assignments ON assignments.id = assignment_id
--- ORDER BY duration;
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (completed_at-started_at) as duration
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN assignments ON assignments.id = assignment_id
+ORDER BY duration;
