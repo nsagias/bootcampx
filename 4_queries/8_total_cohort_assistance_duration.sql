e18a541701d8f8cb84ba1600b4edec5e0729e53a
Nick <nsagias@gmail.com>
2021-10-14 17:49:08 -0400

    fix change

--- 4_queries/8_total_cohort_assistance_duration.sql
+++ 4_queries/8_total_cohort_assistance_duration.sql
@@ -1,9 +1,10 @@
 -- \i 4_queries/8_total_cohort_assistance_duration.sql
 
 -- expected cohort | total_duration 
-SELECT cohorts.name, avg(completed_at - started_at) as total_duration
-FROM assistance_requests 
-JOIN students ON students.id = assistance_requests.student_id
-JOIN cohorts ON cohorts.id = cohort_id
+
+SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts on cohorts.id = cohort_id
 GROUP BY cohorts.name
 ORDER BY total_duration;
