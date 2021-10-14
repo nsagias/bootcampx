6094c486062a6e5aff28a82a46f259ff7367c933
Nick <nsagias@gmail.com>
2021-10-14 09:20:02 -0400

    Add students query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const userInput = process.argv.slice(2);
+const cohortName = userInput[0];
+const outputlimit = userInput[1];
+
+console.log(userInput)
+
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-LIMIT 5;
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
 `)
 .then(res => {
-  console.log(res.rows);
-})
-.catch(err => console.error('query error', err.stack));
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
