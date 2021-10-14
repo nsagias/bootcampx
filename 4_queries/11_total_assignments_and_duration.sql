a50edfb2186bffaaaec41633b3abc747d8800cc2
Nick <nsagias@gmail.com>
2021-10-14 18:28:19 -0400

    Add total assignments and duration query

---
+++ 4_queries/11_total_assignments_and_duration.sql
@@ -0,0 +1,33 @@
+-- \i 4_queries/11_total_assignments_and_duration.sql
+-- Get each day with the total number of assignments and the total duration of the assignments.
+
+-- Select the day, number of assignments, and the total duration of assignments.
+-- Order the results by the day.
+-- Expected Result:
+
+--  day | number_of_assignments | duration 
+-- -----+-----------------------+----------
+--    1 |                    11 |      590
+--    2 |                     9 |      585
+--    3 |                     9 |      425
+--    4 |                     9 |      380
+--    5 |                     7 |      405
+-- ...
+-- (51 rows)
+
+-- Select 
+--   day, 
+--   count(assignments) as number_of_assignments, 
+--   sum(duration) as duration
+-- FROM
+--   assignments
+-- GROUP BY
+--   assignments.day
+-- ORDER BY
+--   day
+-- ;
+
+SELECT day, count(*) as number_of_assignments, sum(duration) as duration
+FROM assignments
+GROUP BY day
+ORDER BY day;
