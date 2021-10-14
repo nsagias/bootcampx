8c24c7f704ef76985aae7be44f10ecd1c65c0095
Nick <nsagias@gmail.com>
2021-10-14 14:18:57 -0400

    Add total student asstiance requests

---
+++ 4_queries/2_total_student_assistance_requests.sql
@@ -0,0 +1,31 @@
+-- \i 4_queries/2_total_student_assistance_requests.sql
+
+
+-- We need to be able to see how many assistance requests any student has requested.
+
+-- Get the total number of assistance_requests for a student.
+
+-- Select the student's name and the total assistance requests.
+-- Since this query needs to work with any specific student name, use 'Elliot Dickinson' for the student's name here.
+-- Expected Result:
+
+
+SELECT 
+  count(assistance_requests.*) as total_assistances, 
+  students.name
+FROM 
+  assistance_requests
+JOIN 
+  students ON students.id = student_id
+WHERE 
+  name = 'Elliot Dickinson'
+GROUP BY 
+ students.name
+;
+
+
+-- SELECT count(assistance_requests.*) as total_assistances, students.name
+-- FROM assistance_requests
+-- JOIN students ON students.id = student_id
+-- WHERE name = 'Elliot Dickinson'
+-- GROUP BY students.name;
